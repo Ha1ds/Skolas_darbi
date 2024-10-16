98bc6e7bed91c85dcd11006179e249e40b3c4d2a
Ha1ds <edijs.balsers@edu.tukums.lv>
2024-10-16 14:08:20 +0300

    .

--- cipari.py
+++ cipari.py
@@ -1,7 +1,14 @@
-import random
-f = open("cipari.txt", "w")
-for i in range(100):
-    x=random.randrange(1,1001)
-    f.write(f"{x}\n")
-    print(x)
+# import random
+# f = open("cipari.txt", "w")
+# for i in range(100):
+#     x=random.randrange(1,1001)
+#     f.write(f"{x}\n")
+#     print(x)
+# f.close()
+
+menesis= ("Janvaris", "Februaris", "Marts", "Aprilis", "Maijs", "Junijs", "Julijs", "Augusts", "Septembris", "Oktobris", "Novembris", "Decembris")
+f= open("cipari.txt", 'w')
+for i in menesis:
+    f.write(i + '\n')
+    print(fails1)
 f.close()
